a205c1a6a25d9dca91180333b53057e2c34da89f
Your Name <you@example.com>
2020-01-28 12:17:44 +0000

    a.java

---
+++ a.java
@@ -0,0 +1,2 @@
+Please note that in most cases the username above will be correct, however please ensure that you read your AMI usage instructions to ensure that the AMI owner has not changed the default AMI username.
+
